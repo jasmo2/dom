4d11aba3282d0d7395b4581d73e575a26088b4ca
jaime <jaime.molina@mattersupply.co>
2020-03-21 06:40:51 -0500

    Exercise

--- public/js/exercises.js
+++ public/js/exercises.js
@@ -1,3 +1,19 @@
 /**
  * Exercises File
+ * Haremos uso del api de Github
+ * ¡ Nuevamente !
+ *
+ * Esta vez usaremos el API de
+ * `https://api.github.com/search/repositories?q=LO_QUE_SE_QUIERA_BUSCAR`
+ *
+ * Donde podremos buscar los repositorios que queramos.
+ *
+ * A Cumplir:
+ * - Hacer un form dinamico como el que tiene Github en su pagina
+ *   cada vez que se escriba se debe hacer una query AJAX al la URL
+ * - Se debe desplegar una lista solo con los 5 primeros resultados con el nombre completo del repo
+ *  -- debe presentarse el nombre completo y
+ *     haber un enalce <a href> con html_url de los componentes en caso de ser escogido
+ *
+ *
  */
