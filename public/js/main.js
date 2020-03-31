5bb49977da1b8668195c33b12331c5634629fbf8
Daniel <dburgos3@eafit.edu.co>
2020-03-31 17:43:31 -0500

    cherry-pick

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,31 @@ const claseCssParrafo = document.querySelectorAll('.clase-css-parrafo')
 console.log('claseCssParrafo', claseCssParrafo)
 const lastPIdx = claseCssParrafo.length - 1
 const lastP = claseCssParrafo[lastPIdx]
+/**
+ * Delegando funciones
+ * Agregando clases y removiendolas
+ */
+const clickClass = (event) => {
+  // se usa function para
+  // poder usar el 'this' que del contexto.
+  // this.classList.toggle('color')
+
+  /**
+   * Aquí verificamos como añadir o remover clases.
+   */
+
+  console.log('TCL: clickClass -> event', event)
+  console.log('TCL: clickClass -> event.currentTarget', event.currentTarget)
+  if (event.currentTarget.classList.contains('color')) {
+    event.currentTarget.classList.remove('color')
+  } else {
+    event.currentTarget.classList.add('color')
+  }
+}
+const clickDivs = document.querySelectorAll('.click-class')
+clickDivs.forEach(clickDiv => clickDiv.addEventListener("click", clickClass))
+
+// const clickDiv = document.querySelector('.click-class')
+
+
+lastP.classList.replace('clase-css-parrafo', 'clase-css-nuevo'), 2
