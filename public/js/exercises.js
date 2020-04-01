7fe6c995e3e545073145509807d6a1815ac79122
jaime <jaime.molina@mattersupply.co>
2020-03-31 19:27:31 -0500

    add to DOM

--- public/js/exercises.js
+++ public/js/exercises.js
@@ -2,8 +2,24 @@
  * Exercises File
  */
 
-const variable = document.querySelectorAll(".divclass")
-debugger
-console.log("TCL: variable", variable)
-debugger
+const variable = document.querySelectorAll('.divclass')
+
+console.log('TCL: variable', variable)
+
 console.log('TCL: urra')
+
+/**
+ * Insertando al DOM
+ */
+
+// Insertando teto
+// con innerHTML
+lastP.innerHTML = `${lastP.innerText} <strong>Con nuevo texto</strong>`
+
+const bodyID = document.querySelector('#bodyID')
+const parrafo = document.createElement('p')
+parrafo.classList.add('added-class')
+parrafo.textContent = 'Texto Interno del nuevo parrafo'
+setTimeout(() => {
+  bodyID.appendChild(parrafo)
+}, 2000)
